b50a6783af4ea7fa132c6ed1886b883ee8f9e378
AnushriPhate <anushrisphate@gmail.com>
2024-02-19 21:40:54 +0530

    HTTP Method Support in Detail Spring Boot

--- SpringMvcProject/src/main/java/com/spring/mvc/SpringMvcProject/controllers/ApiController.java
+++ SpringMvcProject/src/main/java/com/spring/mvc/SpringMvcProject/controllers/ApiController.java
@@ -2,6 +2,7 @@ package com.spring.mvc.SpringMvcProject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +12,21 @@ import java.util.List;
 @RestController //(Controller + ResponseBody)
 @RequestMapping("/api")
 public class ApiController {
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String helloApi()
     {
         return "Hello, how are you?, whats's going there days";
     }
 
-    @RequestMapping("/users")
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
     public List<String> getUserData()
     {
         return Arrays.asList("Ram, Shyam, Chiku");
+    }
+    @RequestMapping(value = "/create-user", method = RequestMethod.POST)
+    public String createUser()
+    {
+        System.out.println("creating user");
+        return "user created...!!";
     }
 }
