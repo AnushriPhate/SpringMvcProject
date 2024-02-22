94bba75b3e59f366ea8439339a5fb525eb7cbf8e
AnushriPhate <anushrisphate@gmail.com>
2024-02-22 22:37:27 +0530

    Working with logger in Spring Boot Project

--- SpringMvcProject/src/main/java/com/spring/mvc/SpringMvcProject/controllers/ProductController.java
+++ SpringMvcProject/src/main/java/com/spring/mvc/SpringMvcProject/controllers/ProductController.java
@@ -1,12 +1,19 @@
 package com.spring.mvc.SpringMvcProject.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.ErrorManager;
+
 @RestController
 public class ProductController {
+
+    Logger logger = LoggerFactory.getLogger(ProductController.class);
+
 //    @GetMapping("/getProduct")
 //    public String getProduct(){
 //        return "This is testing product url";
@@ -14,13 +21,19 @@ public class ProductController {
 
     @GetMapping("/getProduct")
     public String getProduct(
-            @RequestParam("productName") String name,
+            @RequestParam("productName") String productName,
             @RequestParam(value = "productRating", defaultValue = "0", required = false) int rating,
             @RequestParam("productId") int id)
     {
-        System.out.println("ProductName : "+ name);
-        System.out.println("ProductId : " + id);
-        System.out.println("ProductRating : " + rating);
+//        System.out.println("ProductName : "+ name);
+//        System.out.println("ProductId : " + id);
+//        System.out.println("ProductRating : " + rating);
+
+
+        logger.error("ProductName {} {}" , productName, "Testing argument");
+        logger.warn("ProductId {}" , id);
+        logger.info("Product Rating {}" , rating);
+        logger.debug("This is testing for debug");
 
         return "This is testing product url";
     }
